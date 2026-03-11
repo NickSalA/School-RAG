b43c508c2bf958f06172359ca9adf776fc9f429e
Daminin <nicksalcedo717@gmail.com>
2026-03-11 09:22:54 -0500

    feat: Enhance user service and router to include current user context for create, update, and delete operations

--- app/api/routes/user_router.py
+++ app/api/routes/user_router.py
@@ -1,23 +1,30 @@
 """Router para el flujo de usuarios."""
 
+from typing import Annotated
+
 from fastapi import APIRouter, Depends
 from sqlmodel.ext.asyncio.session import AsyncSession
+from app.models import User
 from app.schemas.user_schema import UserRead, UserCreate, UserUpdate
 from app.core.database import get_session
-
+from app.api.dependencies.dep_auth import get_current_user
 from app.services.user_service import UserService
 
 router = APIRouter()
 
+SessionDep = Annotated[AsyncSession, Depends(get_session)]
+CurrentUserDep = Annotated[User, Depends(get_current_user)]
+
 @router.post("/", response_model=UserRead)
-async def create(user_in: UserCreate, session: AsyncSession = Depends(get_session)):
+async def create(user_in: UserCreate, session: SessionDep, current_user: CurrentUserDep):
     """Endpoint para crear un nuevo usuario."""
     service = UserService(session)
-    return await service.create(user_in)
+    return await service.create(user_in, current_user.id)
 
 @router.get("/{user_id}", response_model=UserRead)
-async def get(user_id: int, session: AsyncSession = Depends(get_session)):
+async def get(user_id: int, session: SessionDep):
     """Endpoint para obtener un usuario por su ID."""
+
     service = UserService(session)
     return await service.get(user_id)
 
@@ -28,13 +35,13 @@ async def get_all(session: AsyncSession = Depends(get_session)):
     return await service.list_users()
 
 @router.patch("/{user_id}", response_model=UserRead)
-async def update(user_id: int, user_in: UserUpdate, session: AsyncSession = Depends(get_session)):
+async def update(user_id: int, user_in: UserUpdate, session: SessionDep, current_user: CurrentUserDep):
     """Endpoint para actualizar un usuario existente."""
     service = UserService(session)
-    return await service.update(user_id, user_in)
+    return await service.update(user_id, user_in, current_user.id)
 
 @router.delete("/{user_id}", status_code=204)
-async def delete(user_id: int, session: AsyncSession = Depends(get_session)):
+async def delete(user_id: int, session: SessionDep, current_user: CurrentUserDep):
     """Endpoint para eliminar un usuario."""
     service = UserService(session)
-    await service.delete(user_id)
+    await service.delete(user_id, current_user.id)
--- app/services/user_service.py
+++ app/services/user_service.py
@@ -2,18 +2,32 @@
 
 from sqlmodel.ext.asyncio.session import AsyncSession
 
-from app.repositories import UserRepository
-from app.schemas import UserCreate, UserUpdate, UserRead
+from app.models import ResourceType
+from app.repositories import UserRepository, LogRepository
+from app.schemas import UserCreate, UserUpdate, UserRead, LogCreate
 
 from app.exceptions.auth import UserNotFoundError
 
 class UserService:
     def __init__(self, session: AsyncSession):
         self.user_repo = UserRepository(session)
+        self.log_repo = LogRepository(session)
 
-    async def create(self, user_in: UserCreate) -> UserRead:
+    async def create(self, user_in: UserCreate, user_id: int) -> UserRead:
         """Crea un nuevo usuario."""
         user = await self.user_repo.create(user_in)
+
+        if self.log_repo:
+            log = LogCreate(
+            resource_type=ResourceType.USER,
+            resource_id=user.name,
+            action="create",
+            user_id=user_id,
+            details={"user_id": user.id, "email": user.email, "role": user.role.value}
+        )
+
+            await self.log_repo.create(log)
+
         return UserRead.model_validate(user)
 
     async def get(self, user_id: int) -> UserRead:
@@ -28,18 +42,40 @@ class UserService:
         users = await self.user_repo.get_all()
         return [UserRead.model_validate(u) for u in users]
 
-    async def update(self, user_id: int, user_in: UserUpdate) -> UserRead:
+    async def update(self, user_id: int, user_in: UserUpdate, current_user_id: int) -> UserRead:
         """Actualiza un usuario existente."""
         user = await self.user_repo.get(user_id)
+        if not user:
+            raise UserNotFoundError(f"Usuario con ID {user_id} no encontrado")
+        if self.log_repo:
+            log = LogCreate(
+            resource_type=ResourceType.USER,
+            resource_id=user.name,
+            action="update",
+            user_id=current_user_id,
+            details={"user_id": user.id , "email": user.email, "role": user.role.value}
+        )
+
+            await self.log_repo.create(log)
         if not user:
             raise UserNotFoundError(f"Usuario con ID {user_id} no encontrado")
 
         updated_user = await self.user_repo.update(db_obj=user, obj_in=user_in)
         return UserRead.model_validate(updated_user)
 
-    async def delete(self, user_id: int) -> None:
+    async def delete(self, user_id: int, current_user_id: int) -> None:
         """Elimina un usuario."""
         user = await self.user_repo.get(user_id)
         if not user:
             raise UserNotFoundError(f"Usuario con ID {user_id} no encontrado")
+        if self.log_repo:
+            log = LogCreate(
+            resource_type=ResourceType.USER,
+            resource_id=user.name,
+            action="delete",
+            user_id=current_user_id,
+            details={"user_id": user.id , "email": user.email, "role": user.role.value}
+        )
+
+            await self.log_repo.create(log)
         await self.user_repo.delete(user_id)
